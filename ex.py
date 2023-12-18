b303c58a696d49a9805a14c2cd584e740b878af9
my-account-for <154228793+my-account-for@users.noreply.github.com>
2023-12-18 18:43:33 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,6 +1,22 @@
 import streamlit as st
+from PyPDF2 import PdfReader
+from io import BytesIO
 
 st.write('Hello World')
-x = st.text_input('Link ?')
+file_type = st.text_input('Choose source type [ pdf , audio ]')
 
-st.write("link is:", x)
+if file_type == 'pdf':
+	uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+	if uploaded_file is not None:
+		reader = PdfReader(uploaded_file)
+		page_wise = []
+		full_text = ''
+				
+		for i in range(len(reader.pages)):    
+			p = reader.pages[i]
+			t = p.extract_text()
+			page_wise.append(t.strip())	
+			full_text = full_text + "\n" + t
+
+st.write(full_text)
+    
