412ddd9748ef0a1505539ef284e5c8683ec558c6
my-account-for <154228793+my-account-for@users.noreply.github.com>
2023-12-18 19:30:57 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -3,20 +3,20 @@ import PyPDF2
 from io import BytesIO
 
 st.write('Hello World')
-file_type = st.text_input('Choose source type [ pdf , audio ]')
+# file_type = st.text_input('Choose source type [ pdf , audio ]')
 
-if file_type == 'pdf':
-	uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-	if uploaded_file is not None:
-		reader = PyPDF2.PdfReader(uploaded_file)
-		page_wise = []
-		full_text = ''
+# if file_type == 'pdf':
+# 	uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+# 	if uploaded_file is not None:
+# 		reader = PyPDF2.PdfReader(uploaded_file)
+# 		page_wise = []
+# 		full_text = ''
 				
-		for i in range(len(reader.pages)):    
-			p = reader.pages[i]
-			t = p.extract_text()
-			page_wise.append(t.strip())	
-			full_text = full_text + "\n" + t
+# 		for i in range(len(reader.pages)):    
+# 			p = reader.pages[i]
+# 			t = p.extract_text()
+# 			page_wise.append(t.strip())	
+# 			full_text = full_text + "\n" + t
 
-st.write(full_text)
+# st.write(full_text)
     
