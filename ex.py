cfe26b68680092e498c310742597e4beaf1a2442
my-account-for <154228793+my-account-for@users.noreply.github.com>
2023-12-19 09:55:20 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -6,7 +6,8 @@ import openai
 st.write('Hello User!')
 file_type = st.text_input('Choose source type [ pdf , audio ]')
 max_len_str = st.text_input('Chunk size:')
-max_len = int(max_len_str)
+if max_len_str:
+	max_len = int(max_len_str)
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 full_text = ''
 
