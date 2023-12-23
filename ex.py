07172dbad4b65bb3292dfd8daa737d1b636f6a8f
my-account-for <154228793+my-account-for@users.noreply.github.com>
2023-12-23 18:26:38 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -4,7 +4,7 @@ from io import BytesIO
 import openai
 import pydub
 
-st.latex(r'\raggedright Hello User!')
+st.markdown('# Hello User!')
 
 file_type = st.selectbox(
     'Choose source type [ pdf , audio ]:',
